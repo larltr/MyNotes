9a761a703bf2ba6fef1619a2cf7b9288025aadb6
Zununova <zununiazk@gmail.com>
2023-12-03 15:03:22 +0300

    added two screens

---
+++ app/src/main/java/com/angelika/mynotes/presentation/screens/home/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.angelika.mynotes.presentation.screens.home
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text(text = "Home screen")
+}
